fa416cf89f010e84fcea77eb7eb8c230fea5bfa7
MilovanovicA <alienam994@gmail.com>
2021-11-30 21:30:26 +0100

    prvi zahtev uradjen

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // TODO - Write a get request using path "/" that will return response with the status code 200 and json message 'This is a first request.'"
-
+app.get("/", (req, res) => {
+  res.status(200).json("This is a first request.");
+  });
 // TODO - Write a get request using path "/second" that will return response with the status code 200 and json message 'This is a second request.'"
 
 // TODO - Write a get request using path "/third" that will return response with the status code 200 and json message 'This is a third request.'"
